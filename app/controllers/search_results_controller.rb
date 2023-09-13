1524f2ebb2d790b505a90b26b852dd0b6d0ce401
Weston Schutt <wtschutt@gmail.com>
2023-09-13 11:51:16 -0600

    Test: Add tests for Urgent Services section, with sad path testing

---
+++ app/controllers/search_results_controller.rb
@@ -0,0 +1,22 @@
+class SearchResultsController < ApplicationController
+  
+  def index
+    begin
+      location_added?
+      at_least_one_checked?
+
+    rescue StandardError => e
+      redirect_to root_path
+      flash[:danger] = e.message
+    end
+  end
+
+  private
+  def location_added?
+    raise "Please enter your city, state, and/or zip code" unless !params[:location].blank?
+  end
+
+  def at_least_one_checked?
+    raise "Please select at least one service" unless params[:medical_care] == "1" || params[:crisis_hotline] == "1" || params[:shelter_tonight] == "1" || params[:food] == "1" || params[:substance_use] == "1"
+  end
+end
