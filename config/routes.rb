dc80fa67853299de798981c88276bc2701dc4eac
Weston Schutt <wtschutt@gmail.com>
2023-09-13 11:52:23 -0600

    Feature: Add search results route and controller action, with custom error methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
+  # Register and Login
   namespace :register do
     get '/user', to: 'users#new'
     get '/provider', to: 'providers#new'
   end
-  
   get '/signin', to: 'session#new'
+
+  # Search
+  resources :search_results, only: :index
+
 end
