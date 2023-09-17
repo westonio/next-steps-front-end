519b7d854b5408679ff0ef8b09d8c741abcd64e0
Kaina Cockett <kainacockett@gmail.com>
2023-09-17 09:52:48 -0600

    test: user show page requires login

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   # Register and Login
-  namespace :register do
-    # get '/user', to: 'users#new'
-    get '/provider', to: 'providers#new'
-  end
-  get '/signin', to: 'session#new'
+  # namespace :register do
+  #   # get '/user', to: 'users#new'
+  #   get '/provider', to: 'providers#new'
+  # end
+  # get '/signin', to: 'session#new'
 
   # Search
   resources :search_results, only: [:index, :show]
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
 
   # User features
   resources :users, only: [:index, :show, :new, :create]
+  get "/users/login", to: "users#login"
+  
 
 end
