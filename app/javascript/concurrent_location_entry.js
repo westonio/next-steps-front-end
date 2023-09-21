6ddd0d9652da6ca3b64572ea8b2a0f50739de639
Weston Schutt <wtschutt@gmail.com>
2023-09-21 16:18:12 -0600

    Fix: Add back the geolocation loading text

--- app/javascript/concurrent_location_entry.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
- const locationField = document.getElementById('location');
-  const hiddenLocationField = document.getElementById('hidden-location');
-
-  locationField.addEventListener('input', function() {
-    console.log('Address copied to hidden field');
-    hiddenLocationField.value = this.value;
-  });
-});
